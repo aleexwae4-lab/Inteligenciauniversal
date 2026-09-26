0922f73222bb3fcef9eb86ca162a52ae61785c70
aleexwae4-lab <aleexwae4@gmail.com>
2026-09-25 21:55:01 -0600

    test(game-studio): advance active recovery contract to v14

--- tests/game-studio-v12.test.js
+++ tests/game-studio-v12.test.js
@@ -9,9 +9,9 @@ import {
   STORY_SCHEMA,
   SAVE_SCHEMA,
   LOGIC_SCHEMA
-} from '../lib/game-studio-v12.js';
+} from '../lib/game-studio-v14.js';
 import {inspectFoundryProject,buildDigitalProduct} from '../lib/product-foundry-v5.js';
-import {GAME_STUDIO_VERSION as ACTIVE_GAME_STUDIO_VERSION,inspectGameStudioProject as inspectActiveGameStudioProject} from '../lib/game-studio-v13.js';
+import {GAME_STUDIO_VERSION as ACTIVE_GAME_STUDIO_VERSION,inspectGameStudioProject as inspectActiveGameStudioProject} from '../lib/game-studio-v14.js';
 
 const read=path=>readFileSync(new URL('../'+path,import.meta.url),'utf8');
 
