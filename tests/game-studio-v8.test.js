51db5442aac19fe27503e1594899fddb238fd68a
aleexwae4-lab <aleexwae4@gmail.com>
2026-09-25 21:57:50 -0600

    test(game-studio): bind v8 active recovery alias to v14

--- tests/game-studio-v8.test.js
+++ tests/game-studio-v8.test.js
@@ -4,7 +4,7 @@ import vm from 'node:vm';
 import {readFileSync} from 'node:fs';
 import {createNativeGameStudioProject,inspectGameStudioProject,GAME_STUDIO_VERSION,WORLD_SCHEMA} from '../lib/game-studio-v8.js';
 import {inspectFoundryProject,buildDigitalProduct} from '../lib/product-foundry-v5.js';
-import {GAME_STUDIO_VERSION as ACTIVE_GAME_STUDIO_VERSION,inspectGameStudioProject as inspectActiveGameStudioProject} from '../lib/game-studio-v8.js';
+import {GAME_STUDIO_VERSION as ACTIVE_GAME_STUDIO_VERSION,inspectGameStudioProject as inspectActiveGameStudioProject} from '../lib/game-studio-v14.js';
 
 const read=path=>readFileSync(new URL('../'+path,import.meta.url),'utf8');
 
